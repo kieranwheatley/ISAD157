326cd2619e0446f37f16748f06c02691c814829c
kieranwheatley <56694341+kieranwheatley@users.noreply.github.com>
2020-05-14 12:48:52 +0100

    Added Visual Studio project files
    Uploaded the VS files for a C# interface to connect to my database. It currently displays the Users table and the Education join table.

---
+++ ISAD157 - Database Connection/SOFT562Week18/DatabaseForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace SOFT562Week18
+{
+    partial class DatabaseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewEducation = new System.Windows.Forms.DataGridView();
+            this.dataGridViewUser = new System.Windows.Forms.DataGridView();
+            this.comboBoxQueryChoice = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEducation)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewEducation
+            // 
+            this.dataGridViewEducation.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEducation.Location = new System.Drawing.Point(11, 317);
+            this.dataGridViewEducation.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewEducation.Name = "dataGridViewEducation";
+            this.dataGridViewEducation.RowHeadersWidth = 51;
+            this.dataGridViewEducation.RowTemplate.Height = 24;
+            this.dataGridViewEducation.Size = new System.Drawing.Size(889, 205);
+            this.dataGridViewEducation.TabIndex = 0;
+            // 
+            // dataGridViewUser
+            // 
+            this.dataGridViewUser.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewUser.Location = new System.Drawing.Point(11, 11);
+            this.dataGridViewUser.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewUser.Name = "dataGridViewUser";
+            this.dataGridViewUser.RowHeadersWidth = 51;
+            this.dataGridViewUser.RowTemplate.Height = 24;
+            this.dataGridViewUser.Size = new System.Drawing.Size(889, 302);
+            this.dataGridViewUser.TabIndex = 1;
+            // 
+            // comboBoxQueryChoice
+            // 
+            this.comboBoxQueryChoice.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxQueryChoice.FormattingEnabled = true;
+            this.comboBoxQueryChoice.Items.AddRange(new object[] {
+            "Users",
+            "Educational Institutes"});
+            this.comboBoxQueryChoice.Location = new System.Drawing.Point(11, 526);
+            this.comboBoxQueryChoice.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxQueryChoice.Name = "comboBoxQueryChoice";
+            this.comboBoxQueryChoice.Size = new System.Drawing.Size(889, 29);
+            this.comboBoxQueryChoice.TabIndex = 2;
+            this.comboBoxQueryChoice.SelectedIndexChanged += new System.EventHandler(this.comboBoxQueryChoice_SelectedIndexChanged);
+            // 
+            // DatabaseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(911, 566);
+            this.Controls.Add(this.comboBoxQueryChoice);
+            this.Controls.Add(this.dataGridViewUser);
+            this.Controls.Add(this.dataGridViewEducation);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "DatabaseForm";
+            this.Text = "ISAD157 Database Interface";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEducation)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUser)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewEducation;
+        private System.Windows.Forms.DataGridView dataGridViewUser;
+        private System.Windows.Forms.ComboBox comboBoxQueryChoice;
+    }
+}
+
---
+++ ISAD157 - Database Connection/SOFT562Week18/DatabaseForm.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace SOFT562Week18
+{
+    public partial class DatabaseForm : Form
+    {
+        public DatabaseForm()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBoxQueryChoice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //The if statement uses the top combo box for displaying the list of users
+            if (comboBoxQueryChoice.SelectedIndex == 0)
+            {
+                //The connection string concatenates all the connection information
+                //stored within the DBConnect.cs file ready to establish the connection
+                //with the database.
+                string connectionString = "SERVER=" + DBConnect.SERVER + ";" +
+                    "DATABASE=" + DBConnect.DATABASE_NAME + ";" + "UID=" +
+                    DBConnect.USER_NAME + ";" + "PASSWORD=" +
+                    DBConnect.PASSWORD + ";" + "SslMode=" +
+                    DBConnect.SslMode + ";";
+
+                //Opens a connection with the server
+                using (MySqlConnection connection =
+                    new MySqlConnection(connectionString))
+                {
+                    // Query variable holds the SQL query we want to run, which is to select all users from the users table
+                    string query = "SELECT * FROM isad157_kwheatley.user";
+
+                    //Initiates a connection with the database using the ConnectionString which contains connection information
+                    connection.Open();
+
+                    //Runs the SQL query variables to receive the information
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                    ///////////////////////////////////////////////////////////
+                    //
+                    // The MySqlDataAdapter represents a set of data commands
+                    // and a database connection that are used to fill a
+                    // dataset and update a MySQL database.
+                    //
+                    ///////////////////////////////////////////////////////////
+                    MySqlDataAdapter sqlDA = new MySqlDataAdapter(cmd);
+                    DataTable userDataTable = new DataTable();
+                    sqlDA.Fill(userDataTable);
+
+                    //Binds the user table data to the data
+                    dataGridViewUser.DataSource = userDataTable;
+
+                }
+            }
+            //The else statement uses the bottom combo box to display a list of educationID's along with their name
+            else
+            {
+                /////////////////////////////////////////////////////////////////
+                //
+                // The basic format of a connection string consists of a series
+                // of keyword/value pairs separated by semicolons. The equal
+                // sign (=) connects each keyword and its value. Specifically,
+                // our connection string MUST refer to 
+                // proj-mysql.uopnet.plymouth.ac.uk as the server, and
+                // to our username and password to access
+                // proj-mysql.uopnet.plymouth.ac.uk.
+                //
+                /////////////////////////////////////////////////////////////////
+
+                string connectionString = "SERVER=" + DBConnect.SERVER + ";" +
+                    "DATABASE=" + DBConnect.DATABASE_NAME + ";" + "UID=" +
+                    DBConnect.USER_NAME + ";" + "PASSWORD=" +
+                    DBConnect.PASSWORD + ";" + "SslMode=" +
+                    DBConnect.SslMode + ";";
+
+                ///////////////////////////////////////////////////////////////
+                //
+                // Create an open connection to a MySQL Server database. In our
+                // case, the connection allows us to interact with
+                // proj-mysql.uopnet.plymouth.ac.uk.
+                //
+                ///////////////////////////////////////////////////////////////
+
+                using (MySqlConnection connection =
+                    new MySqlConnection(connectionString))
+                {
+                    ///////////////////////////////////////////////////////////
+                    //
+                    // The query which retrieves all the records in the
+                    // customers table.
+                    //
+                    ///////////////////////////////////////////////////////////
+                    string query = "SELECT * FROM isad157_kwheatley.education";
+
+                    ///////////////////////////////////////////////////////////
+                    //
+                    // Opens a database connection with the property settings
+                    // specified by the ConnectionString.
+                    //
+                    ///////////////////////////////////////////////////////////
+                    connection.Open();
+
+                    ///////////////////////////////////////////////////////////
+                    //
+                    // Submit the SQL statement to be executed against the
+                    // MySQL database.
+                    //
+                    ///////////////////////////////////////////////////////////
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                    ///////////////////////////////////////////////////////////
+                    //
+                    // The MySqlDataAdapter represents a set of data commands
+                    // and a database connection that are used to fill a
+                    // dataset and update a MySQL database.
+                    //
+                    ///////////////////////////////////////////////////////////
+                    MySqlDataAdapter sqlDA = new MySqlDataAdapter(cmd);
+                    DataTable educationDataTable = new DataTable();
+                    sqlDA.Fill(educationDataTable);
+
+                    ///////////////////////////////////////////////////////////
+                    //
+                    // Bind the ACME customer table to the Data Grid View.
+                    //
+                    ///////////////////////////////////////////////////////////
+                    dataGridViewEducation.DataSource = educationDataTable;
+
+                }
+            }
+        }
+    }
+}
